00c84702e9ef3bf49b8223be6add611f4f3fa0fc
retsediv <retsediv1@gmail.com>
2017-09-07 17:42:01 +0300

    Basic login to attack opponent, compute damage and check winner

--- js/app.js
+++ js/app.js
@@ -2,6 +2,53 @@ new Vue({
     el: "#app",
     data: {
         gameStarted: false,
-        logs: []
+        logs: [],
+        playerHealth: 20,
+        computerHealth: 30,
+    },
+    methods: {
+        startGame() {
+            this.playerHealth = 100;
+            this.computerHealth = 100;
+
+            this.gameStarted = true;
+        },
+        attack() {
+            // Attack enemy
+            this.computerHealth -= this.calculateDamage();
+            // Check if player win
+            if (this.checkWin()) {
+                return;
+            }
+
+            // Attack yourself
+            this.playerHealth -= this.calculateDamage();
+            // Check if computer win
+            this.checkWin();
+        },
+
+        checkWin() {
+            if(this.playerHealth <= 0){
+                this.playerHealth = 0;
+                alert("Computer win(");
+
+                return true;
+            } else if (this.computerHealth <= 0){
+                this.computerHealth = 0;
+                alert("You win!");
+
+                return true;
+            }
+
+            return false
+        },
+
+        calculateDamage(min = 5, max = 12) {
+            return parseInt(Math.random() * (max - min) + min);
+        },
+
+        log(message) {
+            this.logs.push(message);
+        }
     }
 });
